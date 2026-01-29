09b45fa408834e42a90b8b796d715cdcbde1cca1
DannyM2027 <daniel.matut2027@gbwl.org>
2026-01-29 19:24:23 +0000

    day 1

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
-   }
-}
+
+     //  1D array is a block of memory that stores a collection
+     // of data items of the same type
+     // UNDER ONE VARIABLE NAME
+
+     // how to declare an ARRAY VARIABLE
+     String[] apCompSci = new string[13];    
+     double[] prices = new double[5]; // [0.0.0.0.0]
+     String[] names = new String[5];
+     int[] luckyNum = new int[10];
+     
+
+
+   } // ENDS MAIN METH
+} // end main class
